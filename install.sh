0fcfd1447698d25bdba07fec7928107fb15415e8
InversionSpaces <InversionSpaces@vivaldi.net>
2019-08-26 19:19:21 +0300

    Added wallpaper

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ git clone https://github.com/tobi-wan-kenobi/bumblebee-status.git i3/bumblebee-s
 git clone https://github.com/gabrielelana/awesome-terminal-fonts.git awesome-fonts
 git clone https://github.com/supermarin/YosemiteSanFranciscoFont.git yosemite-fonts
 
+ln -rs wall.png ~/wall.png
 ln -rs i3/bumblebee-theme.json i3/bumblebee-status/themes/custom.json
 ln -rs awesome-fonts/* ~/.fonts/
 ln -rs yosemite-fonts/*.ttf ~/.fonts/
