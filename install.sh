e57aa4d0c9ccad8247020861451ab0c5f1d4377e
InversionSpaces <InversionSpaces@vivaldi.net>
2019-08-26 18:02:02 +0300

    Some more downloads

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+git clone https://github.com/tobi-wan-kenobi/bumblebee-status.git i3/bumblebee-status
+git clone https://github.com/gabrielelana/awesome-terminal-fonts.git awesome-fonts
+git clone https://github.com/supermarin/YosemiteSanFranciscoFont.git yosemite-fonts
+
+ln -s awesome-fonts/* .fonts/
+ln -s yosemite-fonts/*.ttf .fonts/
 ln -s i3 ~/.config/i3
 ln -s rofi ~/.config/rofi
 ln -s .Xdefault ~/.Xdefaults
-
